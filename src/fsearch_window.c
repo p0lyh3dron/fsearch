9298c21916c432a0606c9402185a597fd97e0fa4
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2021-03-08 15:44:33 +0100

    gtk4: fix Gdk errors

--- src/fsearch_window.c
+++ src/fsearch_window.c
@@ -826,18 +826,18 @@ on_listview_selection_changed(GtkTreeSelection *sel, gpointer user_data) {
 
 static gboolean
 toggle_action_on_2button_press(GdkEvent *event, const char *action, gpointer user_data) {
-    guint button;
-    gdk_event_get_button(event, &button);
-    GdkEventType type = gdk_event_get_event_type(event);
-    if (button != GDK_BUTTON_PRIMARY || type != GDK_2BUTTON_PRESS) {
-        return FALSE;
-    }
-    FsearchApplicationWindow *win = user_data;
-    g_assert(FSEARCH_WINDOW_IS_WINDOW(win));
-    GActionGroup *group = G_ACTION_GROUP(win);
-    GVariant *state = g_action_group_get_action_state(group, action);
-    g_action_group_change_action_state(group, action, g_variant_new_boolean(!g_variant_get_boolean(state)));
-    g_variant_unref(state);
+    // guint button;
+    // gdk_event_get_button(event, &button);
+    // GdkEventType type = gdk_event_get_event_type(event);
+    // if (button != GDK_BUTTON_PRIMARY || type != GDK_2BUTTON_PRESS) {
+    //    return FALSE;
+    //}
+    // FsearchApplicationWindow *win = user_data;
+    // g_assert(FSEARCH_WINDOW_IS_WINDOW(win));
+    // GActionGroup *group = G_ACTION_GROUP(win);
+    // GVariant *state = g_action_group_get_action_state(group, action);
+    // g_action_group_change_action_state(group, action, g_variant_new_boolean(!g_variant_get_boolean(state)));
+    // g_variant_unref(state);
     return TRUE;
 }
 
